ffec010e4940777f43859ae16fdac4761bf6e5db
devty <barjrwork@gmail.com>
2025-09-15 14:04:32 -0500

    Refactor HomePage to stateful and add login fields
    Converted HomePage from StatelessWidget to StatefulWidget and added username and password text fields for login functionality. Updated app bar title and styling. Also added 'svg_flutter' dependency in pubspec.yaml.

--- resq/lib/pages/home.dart
+++ resq/lib/pages/home.dart
@@ -1,16 +1,64 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CrossFadeState _crossFadeState = CrossFadeState.showFirst;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 1), () {
+      if (mounted) {
+        setState(() {
+          _crossFadeState = CrossFadeState.showSecond;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Center(
-          child: Text("Breakfast", style: TextStyle(color: Colors.white)),
+          child: Text(
+            "RESQ",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
         ),
         backgroundColor: Color(0xFFFC3B3C),
+        elevation: 0,
+      ),
+      body: Center(
+        child: SizedBox(
+          width: 250,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                obscureText: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
